453117b2f0338d458668e6cb09a756205228c13b
Akilesh Subhadu <86011577+Yohan4@users.noreply.github.com>
2023-06-02 12:23:30 +0400

    Add files via upload

---
+++ components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li>
+      <input type="checkbox" v-model="todo.completed">
+      <span>{{ todo.task }}</span>
+      <span @click="removeTodo"><i class="fa fa-trash" aria-hidden="true"></i></span>
+    </li>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      todo: {
+        type: Object,
+        required: true,
+      },
+    },
+    methods: {
+      removeTodo() {
+        this.$emit('remove-todo');
+      },
+    },
+  };
+  </script>
+  
+
+  
---
+++ components/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul class="todolist">
+      <li v-for="(todo, index) in todos" :key="index">
+        <input type="checkbox" v-model="todo.completed">
+        <span>{{ todo.task }}</span>
+        <span @click="removeTodo(index)"><i class="fa fa-trash" aria-hidden="true"></i></span>
+      </li>
+    </ul>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      todos: {
+        type: Array,
+        required: true,
+      },
+    },
+    methods: {
+      removeTodo(index) {
+        this.$emit('remove-todo', index);
+      },
+    },
+  };
+  </script>
+  
+ 
+  
